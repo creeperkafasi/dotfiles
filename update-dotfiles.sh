b477db2eaade0a37475d9bd6f3ced14a6d5e80a2
creeperkafasi <49831545+creeperkafasi@users.noreply.github.com>
2021-06-27 22:47:44 +0300

    Upload my current config

---
+++ update-dotfiles.sh
@@ -0,0 +1,7 @@
+cp -r ~/.config ~/dev/dotfiles/.config
+cp -r ~/.i3 ~/dev/dotfiles/.i3
+cp ~/update-dotfiles.sh ~/dev/dotfiles/update-dotfiles.sh
+# cp ~/.bashrc ~/dev/dotfiles/.bashrc
+
+echo Done.
+read -p "Press any key to exit... " -n1 -s
